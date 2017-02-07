26f8bae6d588dab0d0eee0c3de0e80052a19dedf
ak11 <mark@aur.ll.to>
2017-02-07 23:28:56 +0900

    BoardDataからSFENの局面部分への変換処理をpublicメソッドに切り出した

--- ShogiCore/Notation/SFENNotationWriter.cs
+++ ShogiCore/Notation/SFENNotationWriter.cs
@@ -28,39 +28,56 @@ namespace ShogiCore.Notation {
         /// </summary>
         public string WriteToString(Notation notation) {
             StringBuilder str = new StringBuilder();
-            // 初期局面
-            if (notation.InitialBoard == null) {
-                str.Append("startpos");
+            // 局面
+            str.Append(ToString(notation.InitialBoard));
+            // 指し手
+            if (notation.Moves != null && 0 < notation.Moves.Length) {
+                str.Append(" moves");
+                foreach (MoveDataEx t in notation.Moves) {
+                    str.Append(' ');
+                    str.Append(ToString(t.MoveData));
+                }
+            }
+            return str.ToString();
+        }
+
+        /// <summary>
+        /// SFEN棋譜の書き出し(末尾に改行無しの文字列で返す)
+        /// </summary>
+        public static string ToString(BoardData boardData, bool useStartPos = true) {
+            if (boardData == null) {
+                // 初期局面
+                return useStartPos ? "startpos" : "sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1";
             } else {
+                StringBuilder str = new StringBuilder();
                 str.Append("sfen ");
                 // 盤面
                 for (int rank = 1; rank <= 9; rank++) {
                     for (int file = 9; 1 <= file; file--) { // １段目の左側（９筋側）から駒の種類を書いていきます
-                        if (notation.InitialBoard[file, rank] == Piece.EMPTY) {
+                        if (boardData[file, rank] == Piece.EMPTY) {
                             int count = 1;
                             for (file--; 1 <= file; file--) {
-                                if (notation.InitialBoard[file, rank] != Piece.EMPTY) break;
+                                if (boardData[file, rank] != Piece.EMPTY) break;
                                 count++;
                             }
                             file++; // 1個戻す。(´ω`)
                             str.Append(count);
                         } else {
-                            str.Append(SFENNotationReader.ToName(
-                                notation.InitialBoard[file, rank]));
+                            str.Append(SFENNotationReader.ToName(boardData[file, rank]));
                         }
                     }
                     if (rank < 9) str.Append('/');
                 }
                 str.Append(' ');
                 // 手番
-                str.Append("bw"[notation.InitialBoard.Turn]);
+                str.Append("bw"[boardData.Turn]);
                 str.Append(' ');
                 // 持ち駒
                 {
                     string handStr = "";
                     for (int t = 0; t < 2; t++) {
                         Piece turnMask = t == 0 ? Piece.EMPTY : Piece.ENEMY;
-                        int[] hand = notation.InitialBoard.GetHand(t);
+                        int[] hand = boardData.GetHand(t);
                         for (Piece p = Piece.FU; p < Piece.OU; p++) {
                             int n = hand[(byte)p];
                             if (0 < n) {
@@ -83,21 +100,13 @@ namespace ShogiCore.Notation {
                 // 手数
                 str.Append('1');
                 // startpos化
-                if (string.CompareOrdinal(str.ToString(),
+                if (useStartPos && string.CompareOrdinal(str.ToString(),
                     "sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1") == 0) {
                     str.Length = 0;
                     str.Append("startpos");
                 }
+                return str.ToString();
             }
-            // 指し手
-            if (notation.Moves != null && 0 < notation.Moves.Length) {
-                str.Append(" moves");
-                foreach (MoveDataEx t in notation.Moves) {
-                    str.Append(' ');
-                    str.Append(ToString(t.MoveData));
-                }
-            }
-            return str.ToString();
         }
 
         /// <summary>
